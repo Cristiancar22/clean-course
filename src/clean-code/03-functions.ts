2ef889e1f5a729aeabc042af553624530443f10b
Cristian <cristian.cardenas@esmovil.es>
2024-11-07 11:35:51 +0100

    upda

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -7,3 +7,46 @@ const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false })
     return ( isRetired ) ? 3000 : 4000;
     
 }
+(() => {
+
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function geActorBioById( id: string ) {
+        console.log({ id });
+    }
+
+    interface Movie {
+        cast:        string[]
+        description: string, 
+        rating:      number, 
+        title:       string, 
+    }
+
+    function createMovie({ title, description, rating, cast }: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor', birthdate);
+        return true;        
+
+    }
+
+})();
+
+
+
+
